9a89df97f3a1ae6e0380fe552548129008611cbd
nguyen <nguyenkdksnb@gmail.com>
2021-09-27 10:22:35 +0700

    update gg button,helps and return button and scroll bar

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
 
   public void start(Stage Primarystage) {
     Primarystage.setTitle("Dictionary");
+    Primarystage.setResizable(false);
     try {
       FXMLLoader loader = new FXMLLoader();
       Parent root = loader.load(this.getClass().getResource("frontend/Controller.fxml"));
