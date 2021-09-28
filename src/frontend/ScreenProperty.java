4501a43a6b6a3efc54ccbc089aae62a50d06ddd9
nguyen <nguyenkdksnb@gmail.com>
2021-09-28 22:00:28 +0700

    Add hover effects to the buttons Add click effects to the buttons Edit translate area

--- src/frontend/ScreenProperty.java
+++ src/frontend/ScreenProperty.java
@@ -9,17 +9,22 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.web.HTMLEditor;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 public class ScreenProperty implements Initializable {
   // public TextField height ;
   public TextField inputText;
-  public Label label = new Label();
+  // public Label label = new Label();
   public String inputString = new String();
   public String meaning = "";
   public String VNtrans = "";
-  // public HTMLEditor htmlEditor;
+  public WebView webView;
+  // WebEngine engine = webView.getEngine();
+  public HTMLEditor htmlEditor;
 
   /**
    * Submit text to translate.
@@ -31,13 +36,37 @@ public class ScreenProperty implements Initializable {
     // TranslateApi newTrans = new TranslateApi();
     inputString = inputText.getText();
 
+
+
     try {
       final Database definition = new Database();
       meaning = definition.searchWord(inputString);
-      label.setText(meaning);
+      // label.setText(meaning);
+      setHTML(meaning);
+      HTMLtoWebview(htmlEditor);
+      // engine.loadContent(meaning, "text/html");
     } catch (SQLException e) {
-      e.printStackTrace();
+      e.getMessage();
+      Alert alert = new Alert(Alert.AlertType.INFORMATION);
+      alert.setContentText("Word not found. Please use Google Translate.");
+      alert.show();
+
     }
+
+
+  }
+
+  public void setHTML(String s) {
+    htmlEditor.setHtmlText(s);
+    // engine.loadContent(htmlEditor.getHtmlText(), "text/html");
+  }
+
+  public void HTMLtoWebview(HTMLEditor aEditor) {
+    WebEngine webEngine = webView.getEngine();
+    webEngine.loadContent(aEditor.getHtmlText(), "text/html");
+  }
+
+  public void initialize(URL location, ResourceBundle resources) {
 
   }
 
@@ -50,7 +79,9 @@ public class ScreenProperty implements Initializable {
     TranslateApi newTrans = new TranslateApi();
     try {
       VNtrans = newTrans.translate(inputString);
-      label.setText(VNtrans);
+      setHTML(VNtrans);
+      HTMLtoWebview(htmlEditor);
+      // label.setText(VNtrans);
     } catch (UnirestException e) {
       // TODO
       System.out.println("Out of network");
@@ -60,6 +91,5 @@ public class ScreenProperty implements Initializable {
 
   private void printStackTrace() {}
 
-  public void initialize(URL location, ResourceBundle resources) {}
 
 }
