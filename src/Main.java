e2cae56f36a7a19cc6d21c57f859a4d3e3f5ac91
Thinh Tran <thinhrom11@gmail.com>
2021-10-07 17:40:17 +0700

    Delete unused files

--- src/Main.java
+++ src/Main.java
@@ -41,7 +41,6 @@ public class Main extends Application {
       primaryStage.setScene(scene);
       primaryStage.show();
       primaryStage.setOnCloseRequest(arg0 -> {
-        MyLogger.getLogger().info("End of program");
         Platform.exit();
         System.exit(0);
       });
